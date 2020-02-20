f4b39b38d44146eb20b1912d74032bdb03d94452
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-20 18:47:07 -0500

    for loop

---
+++ module2/program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int condition1;
+
+            int condition1 = 3; 
+            if (condition1 == 3)
+            {
+                Console.WriteLine("works");
+            }
+            else 
+            {
+                Console.WriteLine("nah");
+            }
+        }
+    }
+}
