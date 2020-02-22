0f24a68c89943c7df8c6e012678cade1135b1947
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-22 12:19:02 -0500

    forloop

---
+++ module2/mod2lab3.cs
@@ -0,0 +1,8 @@
+ // Create a simple for loop that displays the values of the counter
+            // The WriteLine method here demonstrates the use of string interpolation in C#
+            // as a way of outputting literal string values with variable values.
+            // It is the recommended way to deal with this method of string output
+            for(int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Counter is at: {counter}"); 
+            }
